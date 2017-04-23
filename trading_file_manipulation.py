949ceb63773440fe6de32e9fbbaaacb6cce0ae72
rjaglal <ravi.jaglal@gmail.com>
2017-04-23 15:16:45 -0400

    first read of open source file

--- trading_file_manipulation.py
+++ trading_file_manipulation.py
@@ -1,6 +1,14 @@
+import pandas as pd
+
+
+def read_csv():
+    df = pd.read_csv("data_files/comtrade.csv")
+    print(df['Reporter'][10:21])
+
+
 def hello_world():
     print("Hello World")
-    print("Next try")
 
 if __name__ == "__main__":
     hello_world()
+    read_csv()
